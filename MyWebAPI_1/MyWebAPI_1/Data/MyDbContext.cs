676ebb2a5519f06dcf29cc87d65a8cd9c5f794f8
Hieu <honghieutd@gmail.com>
2022-12-19 17:19:14 +0700

    Add Model Loai

---
+++ MyWebAPI_1/MyWebAPI_1/Data/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebAPI_1.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions options) : base(options) { }
+
+        #region DbSet
+        public DbSet<HangHoa> HangHoas { get; set; }
+        public DbSet<Loai> Loais { get; set; }
+
+        #endregion
+    }
+}
---
+++ MyWebAPI_1/MyWebAPI_1/Models/LoaiModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebAPI_1.Models
+{
+    public class LoaiModel
+    {
+        //Ma tu tang nen chi can ten loai thoi
+        [Required]
+        [MaxLength(50)]
+        public string TenLoai { get; set; }
+    }
+}
