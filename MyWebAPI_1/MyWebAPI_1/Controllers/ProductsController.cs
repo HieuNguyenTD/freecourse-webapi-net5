2801f8d291bb4b518b64e20ed289c3424a5ce295
Hieu Nguyen <honghieutd@gmail.com>
2022-12-21 10:48:26 +0700

    Filtering Sorting Paging

---
+++ MyWebAPI_1/MyWebAPI_1/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebAPI_1.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebAPI_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IHangHoaRepository _hangHoaRepository;
+        public ProductsController(IHangHoaRepository hangHoaRepository)
+        {
+            _hangHoaRepository = hangHoaRepository;
+        }
+
+        //[HttpGet]
+        //public IActionResult GetAll()
+        //{
+        //    try
+        //    {
+        //        var result = _hangHoaRepository.GetAll();
+        //        return Ok(result);
+        //    }
+        //    catch
+        //    {
+        //        return BadRequest("We can't get the product.");
+        //    }
+        //}
+
+        [HttpGet]
+        public IActionResult GetAllProducts(string search, double? from, double? to, 
+            string sortBy = "gia_asc", int page = 1)
+        {
+            try
+            {
+                var result = _hangHoaRepository.GetAll(search, from, to, sortBy, page);
+                return Ok(result);
+            }
+            catch
+            {
+                return BadRequest("We can't get the product.");
+            }
+        }
+    }
+}
---
+++ MyWebAPI_1/MyWebAPI_1/Services/HangHoaRepository.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyWebAPI_1.Data;
+using MyWebAPI_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebAPI_1.Services
+{
+    public class HangHoaRepository : IHangHoaRepository
+    {
+        private readonly MyDbContext _context;
+        private static int PAGE_SIZE { get; set; } = 5;
+        public HangHoaRepository(MyDbContext context)
+        {
+            _context = context;
+        }
+        public List<HangHoaModel> GetAll(string search, double? from, double? to, string sortBy, int page = 1)
+        {
+            //Muốn ra data của khóa ngoại thì phải có Include
+            var allProducts = _context.HangHoas.Include(hh=>hh.Loai).AsQueryable();
+
+            #region Filtering
+            if (!string.IsNullOrEmpty(search))
+            {
+                allProducts = allProducts.Where(hh => hh.TenHh.Contains(search));
+            }
+
+            if (from.HasValue)
+            {
+                allProducts = allProducts.Where(hh => hh.DonGia >= from);
+            }
+
+            if (to.HasValue)
+            {
+                allProducts = allProducts.Where(hh => hh.DonGia <= to);
+            }
+            #endregion
+
+            #region Sorting
+            //Default sort by Name (TenHh)
+            allProducts = allProducts.OrderBy(hh => hh.TenHh);
+
+            if (!string.IsNullOrEmpty(sortBy))
+            {
+                switch (sortBy)
+                {
+                    case "tenhh_desc":
+                        allProducts = allProducts.OrderByDescending(hh => hh.TenHh);
+                        break;
+                    case "gia_asc":
+                        allProducts = allProducts.OrderBy(hh => hh.DonGia);
+                        break;
+                    case "gia_desc":
+                        allProducts = allProducts.OrderByDescending(hh => hh.DonGia);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            #endregion
+
+            //#region Paging
+            //allProducts = allProducts.Skip((page - 1) * PAGE_SIZE).Take(PAGE_SIZE);
+            //#endregion
+
+            //var result = allProducts.Select(hh => new HangHoaModel
+            //{
+            //    MaHangHoa = hh.MaHh,
+            //    TenHangHoa = hh.TenHh,
+            //    DonGia = hh.DonGia,
+            //    TenLoai = hh.Loai.TenLoai
+            //});
+
+            //return result.ToList();
+
+            var result = PaginatedList<MyWebAPI_1.Data.HangHoa>.Create(allProducts, page, PAGE_SIZE);
+            return result.Select(hh => new HangHoaModel
+            {
+                MaHangHoa = hh.MaHh,
+                TenHangHoa = hh.TenHh,
+                DonGia = hh.DonGia,
+                TenLoai = hh.Loai?.TenLoai
+            }).ToList();
+        }
+
+        public List<HangHoaModel> GetAll_bk(string search, double? from, double? to, string sortBy, int page = 1)
+        {
+            var allProducts = _context.HangHoas.AsQueryable();
+
+            #region Filtering
+            if (!string.IsNullOrEmpty(search))
+            {
+                allProducts = allProducts.Where(hh => hh.TenHh.Contains(search));
+            }
+
+            if (from.HasValue)
+            {
+                allProducts = allProducts.Where(hh => hh.DonGia >= from);
+            }
+
+            if (to.HasValue)
+            {
+                allProducts = allProducts.Where(hh => hh.DonGia <= to);
+            }
+            #endregion
+
+            #region Sorting
+            //Default sort by Name (TenHh)
+            allProducts = allProducts.OrderBy(hh => hh.TenHh);
+
+            if (!string.IsNullOrEmpty(sortBy))
+            {
+                switch (sortBy)
+                {
+                    case "tenhh_desc":
+                        allProducts = allProducts.OrderByDescending(hh => hh.TenHh);
+                        break;
+                    case "gia_asc":
+                        allProducts = allProducts.OrderBy(hh => hh.DonGia);
+                        break;
+                    case "gia_desc":
+                        allProducts = allProducts.OrderByDescending(hh => hh.DonGia);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            #endregion
+
+            #region Paging
+            allProducts = allProducts.Skip((page - 1) * PAGE_SIZE).Take(PAGE_SIZE);
+            #endregion
+
+            var result = allProducts.Select(hh => new HangHoaModel
+            {
+                MaHangHoa = hh.MaHh,
+                TenHangHoa = hh.TenHh,
+                DonGia = hh.DonGia,
+                TenLoai = hh.Loai.TenLoai
+            });
+
+            return result.ToList();
+
+
+        }
+
+        //public List<HangHoaModel> GetAll(string search)
+        //{
+        //    var allProducts = _context.HangHoas.Where(hh => hh.TenHh.Contains(search));
+
+        //    var result = allProducts.Select(hh => new HangHoaModel
+        //    {
+        //        MaHangHoa = hh.MaHh,
+        //        TenHangHoa = hh.TenHh,
+        //        DonGia = hh.DonGia,
+        //        TenLoai = hh.Loai.TenLoai
+        //    });
+
+        //    return result.ToList();
+        //}
+
+        public List<HangHoaModel> GetAll()
+        {
+            var allProducts = _context.HangHoas.ToList();
+
+            var result = allProducts.Select(hh => new HangHoaModel
+            {
+                MaHangHoa = hh.MaHh,
+                TenHangHoa = hh.TenHh,
+                DonGia = hh.DonGia,
+                TenLoai = hh.Loai.TenLoai
+            });
+
+            return result.ToList();
+        }
+    }
+}
